93166d8065ec32565d42f68816dd9ea72061fcfb
Leo <leandrofbermeo@gmail.com>
2015-02-23 17:05:33 +0000

    Updates

--- js/eventscripts.js
+++ js/eventscripts.js
@@ -11,3 +11,21 @@ $('#toggle').click(function(){
   $('.panel-collapse:not(".in")')
     .collapse('show');
 });
+
+
+
+// Bootstrap Speakers Live Search
+$(document).ready(function(){
+	$(".search").keyup(function(){
+		var str = $(".search").val();
+		$(".speaker-listing").each(function(index){
+			if($(this).attr("id")){
+				if(!$(this).attr("id").match(new RegExp(str, "i"))){
+					$(this).fadeOut("10");
+ 				}else{
+					$(this).fadeIn("10");
+ 				}
+			}
+		});		
+	});
+});
