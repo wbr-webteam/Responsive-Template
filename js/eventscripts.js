79cc2df1b586bd0a0a420130449e7f5ce4edbb15
Joshua Vining <JoshuaVining86@gmail.com>
2015-08-19 16:03:30 -0400

    Toggle JS updated
    - added plus/minus toggles dynamically
    - attached plus/minus toggles to show more/less
    - added plus/minus independent toggle

--- js/eventscripts.js
+++ js/eventscripts.js
@@ -1,15 +1,38 @@
 //Agenda Show toggle
 $(document).ready(function(){
+	// Dyanmically add the plus/minus toggle to the header 
+	$('.panel-collapse').has('.panel-body').prev().children('h4').prepend('<i class="fa fa-plus-circle text-grey"></i> ');
+  
 	$('#toggle').click(function() {
 	    if ($('.panel-collapse').hasClass('in')){
 	        $('.panel-collapse').removeClass('in');  
-			$('#toggle').html('+ Show More');  
+			$('#toggle').html('+ Show More'); 
+			$('.panel-title i').toggleClass('fa-minus-circle').toggleClass('fa-plus-circle');
+	     
 	    } else {
 	        $('.panel-collapse').addClass('in');
-			$('#toggle').html('- Show Less');  
-	      }
+			$('#toggle').html('- Show Less'); 
+			$('.panel-title i').toggleClass('fa-minus-circle').toggleClass('fa-plus-circle');
+  	      }
 	});
 });
+
+// Handles the toggle of a single onclick panel event 
+$(document).ready(function() {
+   $(".panel-title").click(function() {
+	  $(this).children().toggleClass('fa-minus-circle');
+   });
+});
+
+
+ 
+ 
+
+
+
+
+
+
 
 // Bootstrap Speakers Live Search
 $(document).ready(function(){
